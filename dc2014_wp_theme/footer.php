9c1e24b0963d15dc2586e5d632cac22a6646ecfd
Svebor Prstačić <svebor@open.hr>
2014-04-09 17:13:31 +0200

    Added Google Analytics

--- dc2014_wp_theme/footer.php
+++ dc2014_wp_theme/footer.php
@@ -22,6 +22,17 @@
         <div class="info info2 bn f18 lg pwdb">POWERED BY: Free and open source software!</div>
         </div>
         </div>
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-38450003-1', 'dorscluc.org');
+	  ga('send', 'pageview');
+
+	</script>
 
     </body>
 </html>
