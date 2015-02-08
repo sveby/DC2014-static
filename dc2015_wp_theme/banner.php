5fe5ea07cdbb70c4f129fa76a45e6fcd5cd6cccb
Jure Rastic <jure@rastic.net>
2015-02-08 16:20:38 +0100

    dodao ispravke na banner-u i za HR verziju

--- dc2015_wp_theme/banner.php
+++ dc2015_wp_theme/banner.php
@@ -17,7 +17,7 @@
                     <div class="b_txt f36 w sponsor-title">
                         <a id="secondary_open_sponsorship_dialog" class="w">Want to be a sponsor?</a>
                     </div>
-                    <div class="n_txt f14 w">Want to connect with your users or introduce yourself to the open source community?<br>There is no better place than the biggest croatian open technology conference.</div>
+                    <div class="n_txt f13 w">Want to connect with your users or introduce yourself to the open source community?<br>There is no better place than the biggest croatian open technology conference.</div>
                     <div class="m_txt" style="margin-top:15px;margin-bottom:10px;"><a class="f25 w t_d_u"  href="mailto:info@dorscluc.org">contact us!</a></div>
                     <!--<div class="pos_sponsor bm_txt w">or <a class="f18 w" href>contact</a> us the way you prefer :)</div>-->
                 </div>
@@ -58,20 +58,19 @@
 
         <div class="img_main">
             <!--<div class="header">-->
-            <!--img class="img_main" src="back.jpg"-->
-            <div class="main_logo"><a href="http://2015.dorscluc.org"><img class="main_img" src="<?php bloginfo('template_url');?>/main_logo.png"></a>
+            <img class="img_main_back" src="<?php bloginfo('template_url');?>/back.jpg">
+            <div class="main_logo"><a href="http://2015.dorscluc.org"><img src="<?php bloginfo('template_url');?>/main_logo.png"></a>
             <br>
                 <span class="date">18. do 20. svibnja, 2015</span><span class="sub_date"><br>Zagreb</span>
                 <br>
             </div>
 
             <div class="pos_sponsor sponsor">
-                <div class="pos_sponsor_background"></div>
                 <div class="pos_sponsor_overlay">
                     <div class="pos_sponsor b_txt f42 w">
                         <a id="secondary_open_sponsorship_dialog" class="w">Zainteresirani za sponzorstvo?</a>
                     </div>
-                    <div class="pos_sponsor n_txt f14 w">Želite se povezati s korisnicima ili predstaviti zajednici?<br>DORS/CLUC je idealno mjesto za to!</div>
+                    <div class="pos_sponsor n_txt f13 w">Želite se povezati s korisnicima ili predstaviti zajednici?<br>DORS/CLUC je idealno mjesto za to!</div>
                     <div class="pos_sponsor m_txt"><a class="f25 w" href="mailto:info@dorscluc.org">kontaktirajte nas!</a></div>
                 </div>
             </div>
